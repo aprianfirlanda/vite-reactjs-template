680b705a25186bf072f7da2a219ecea27581a87c
aprianfirlanda <aprianfirlanda@gmail.com>
2025-09-05 20:51:52 +0700

    style(eslint and prettier): setup prettier that matches with eslint. also fix lint error and adjust formats on code

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -1,73 +1,72 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 const textClasses = {
-  xs: "text-xs",
-  sm: "text-sm",
-  md: "text-sm",
-  lg: "text-sm",
-  xl: "text-sm",
+  xs: 'text-xs',
+  sm: 'text-sm',
+  md: 'text-sm',
+  lg: 'text-sm',
+  xl: 'text-sm',
 };
 
 const radiusNormal = {
-  xs: "rounded-sm",
-  sm: "rounded-sm",
-  md: "rounded-md",
-  lg: "rounded-md",
-  xl: "rounded-md",
+  xs: 'rounded-sm',
+  sm: 'rounded-sm',
+  md: 'rounded-md',
+  lg: 'rounded-md',
+  xl: 'rounded-md',
 };
 
 const paddingByShape = {
   normal: {
-    xs: "px-2 py-1",
-    sm: "px-2 py-1",
-    md: "px-2.5 py-1.5",
-    lg: "px-3 py-2",
-    xl: "px-3.5 py-2.5",
+    xs: 'px-2 py-1',
+    sm: 'px-2 py-1',
+    md: 'px-2.5 py-1.5',
+    lg: 'px-3 py-2',
+    xl: 'px-3.5 py-2.5',
   },
   rounded: {
-    xs: "px-2.5 py-1",
-    sm: "px-2.5 py-1",
-    md: "px-3 py-1.5",
-    lg: "px-3.5 py-2",
-    xl: "px-4 py-2.5",
+    xs: 'px-2.5 py-1',
+    sm: 'px-2.5 py-1',
+    md: 'px-3 py-1.5',
+    lg: 'px-3.5 py-2',
+    xl: 'px-4 py-2.5',
   },
   circular: {
-    xs: "p-1",
-    sm: "p-1.5",
-    md: "p-2",
-    lg: "p-2.5",
-    xl: "p-3",
+    xs: 'p-1',
+    sm: 'p-1.5',
+    md: 'p-2',
+    lg: 'p-2.5',
+    xl: 'p-3',
   },
 };
 
 const variantClasses = {
   normal:
-    "bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600",
-  outlined:
-    "bg-white text-gray-900 inset-ring inset-ring-gray-300 hover:bg-gray-50",
-  soft: "bg-indigo-50 text-indigo-600 hover:bg-indigo-100",
+    'bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+  outlined: 'bg-white text-gray-900 inset-ring inset-ring-gray-300 hover:bg-gray-50',
+  soft: 'bg-indigo-50 text-indigo-600 hover:bg-indigo-100',
 };
 
 const iconSizes = {
-  xs: "size-3",
-  sm: "size-3",
-  md: "size-4",
-  lg: "size-4",
-  xl: "size-5",
+  xs: 'size-3',
+  sm: 'size-3',
+  md: 'size-4',
+  lg: 'size-4',
+  xl: 'size-5',
 };
 
 // note -> shape(circular) only for the button that fills with icon only
-function Button({ children, size = "md", shape = "normal", variant = "normal" }) {
-  const radius = shape === "normal" ? radiusNormal[size] : "rounded-full";
+function Button({ children, size = 'md', shape = 'normal', variant = 'normal' }) {
+  const radius = shape === 'normal' ? radiusNormal[size] : 'rounded-full';
   const padding = paddingByShape[shape][size];
-  const text = shape === "circular" ? "" : textClasses[size];
+  const text = shape === 'circular' ? '' : textClasses[size];
   const iconSize = iconSizes[size];
 
   const processedChildren = React.Children.map(children, (child) => {
     if (React.isValidElement(child)) {
       return React.cloneElement(child, {
-        className: `${iconSize} ${child.props.className || ""}`,
+        className: `${iconSize} ${child.props.className || ''}`,
       });
     }
     return child;
@@ -84,9 +83,9 @@ function Button({ children, size = "md", shape = "normal", variant = "normal" })
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
-  size: PropTypes.oneOf(["xs", "sm", "md", "lg", "xl"]),
-  shape: PropTypes.oneOf(["normal", "rounded", "circular"]),
-  variant: PropTypes.oneOf(["normal", "outlined", "soft"]),
+  size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  shape: PropTypes.oneOf(['normal', 'rounded', 'circular']),
+  variant: PropTypes.oneOf(['normal', 'outlined', 'soft']),
 };
 
 export default Button;
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,71 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {Button} from "./components/ui/button/index.js";
-import {PlusIcon} from "@heroicons/react/24/outline/index.js";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Button } from './components/ui/button/index.js';
+import { PlusIcon } from '@heroicons/react/24/outline/index.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className="mx-auto w-fit">
-      <Button size="xs"><PlusIcon/>Add</Button>
-      <Button size="sm"><PlusIcon/>Add</Button>
-      <Button size="md"><PlusIcon/>Add</Button>
-      <Button size="lg"><PlusIcon/>Add</Button>
-      <Button size="xl"><PlusIcon/>Add</Button>
+      <Button size="xs">
+        <PlusIcon />
+        Add
+      </Button>
+      <Button size="sm">
+        <PlusIcon />
+        Add
+      </Button>
+      <Button size="md">
+        <PlusIcon />
+        Add
+      </Button>
+      <Button size="lg">
+        <PlusIcon />
+        Add
+      </Button>
+      <Button size="xl">
+        <PlusIcon />
+        Add
+      </Button>
     </div>
     <div className="mx-auto w-fit">
-      <Button size="xs" shape="rounded"><PlusIcon/>Add</Button>
-      <Button size="sm" shape="rounded"><PlusIcon/>Add</Button>
-      <Button size="md" shape="rounded"><PlusIcon/>Add</Button>
-      <Button size="lg" shape="rounded"><PlusIcon/>Add</Button>
-      <Button size="xl" shape="rounded"><PlusIcon/>Add</Button>
+      <Button size="xs" shape="rounded">
+        <PlusIcon />
+        Add
+      </Button>
+      <Button size="sm" shape="rounded">
+        <PlusIcon />
+        Add
+      </Button>
+      <Button size="md" shape="rounded">
+        <PlusIcon />
+        Add
+      </Button>
+      <Button size="lg" shape="rounded">
+        <PlusIcon />
+        Add
+      </Button>
+      <Button size="xl" shape="rounded">
+        <PlusIcon />
+        Add
+      </Button>
     </div>
     <div className="mx-auto w-fit">
-      <Button size="xs" shape="circular"><PlusIcon/></Button>
-      <Button size="sm" shape="circular"><PlusIcon/></Button>
-      <Button size="md" shape="circular"><PlusIcon/></Button>
-      <Button size="lg" shape="circular"><PlusIcon/></Button>
-      <Button size="xl" shape="circular"><PlusIcon/></Button>
+      <Button size="xs" shape="circular">
+        <PlusIcon />
+      </Button>
+      <Button size="sm" shape="circular">
+        <PlusIcon />
+      </Button>
+      <Button size="md" shape="circular">
+        <PlusIcon />
+      </Button>
+      <Button size="lg" shape="circular">
+        <PlusIcon />
+      </Button>
+      <Button size="xl" shape="circular">
+        <PlusIcon />
+      </Button>
     </div>
-  </StrictMode>,
-)
+  </StrictMode>
+);
