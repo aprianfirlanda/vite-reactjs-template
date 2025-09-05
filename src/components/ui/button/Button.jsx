aa8e2ab4f4a26ca6d56e1e87b76d7ef67866cfc0
aprianfirlanda <aprianfirlanda@gmail.com>
2025-09-05 20:14:26 +0700

    feat(ui button): add prop shape

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-const sizeClasses = {
-  xs: "rounded-sm px-2 py-1 text-xs",
-  sm: "rounded-sm px-2 py-1 text-sm",
-  md: "rounded-md px-2.5 py-1.5 text-sm",
-  lg: "rounded-md px-3 py-2 text-sm",
-  xl: "rounded-md px-3.5 py-2.5 text-sm",
+const textClasses = {
+  xs: "text-xs",
+  sm: "text-sm",
+  md: "text-sm",
+  lg: "text-sm",
+  xl: "text-sm",
+};
+
+const radiusNormal = {
+  xs: "rounded-sm",
+  sm: "rounded-sm",
+  md: "rounded-md",
+  lg: "rounded-md",
+  xl: "rounded-md",
+};
+
+const paddingByShape = {
+  normal: {
+    xs: "px-2 py-1",
+    sm: "px-2 py-1",
+    md: "px-2.5 py-1.5",
+    lg: "px-3 py-2",
+    xl: "px-3.5 py-2.5",
+  },
+  rounded: {
+    xs: "px-2.5 py-1",
+    sm: "px-2.5 py-1",
+    md: "px-3 py-1.5",
+    lg: "px-3.5 py-2",
+    xl: "px-4 py-2.5",
+  },
+  circular: {
+    xs: "p-1",
+    sm: "p-1.5",
+    md: "p-2",
+    lg: "p-2.5",
+    xl: "p-3",
+  },
 };
 
 const variantClasses = {
@@ -17,10 +49,15 @@ const variantClasses = {
   soft: "bg-indigo-50 text-indigo-600 hover:bg-indigo-100",
 };
 
-function Button({ children, size = "md", variant = "normal" }) {
+// note -> shape(circular) only for the button that fills with icon only
+function Button({ children, size = "md", shape = "normal", variant = "normal" }) {
+  const radius = shape === "normal" ? radiusNormal[size] : "rounded-full";
+  const padding = paddingByShape[shape][size];
+  const text = shape === "circular" ? "" : textClasses[size];
+
   return (
     <button
-      className={`font-semibold shadow-xs ${sizeClasses[size]} ${variantClasses[variant]}`}
+      className={`font-semibold shadow-xs ${radius} ${padding} ${text} ${variantClasses[variant]}`}
     >
       {children}
     </button>
@@ -30,6 +67,7 @@ function Button({ children, size = "md", variant = "normal" }) {
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   size: PropTypes.oneOf(["xs", "sm", "md", "lg", "xl"]),
+  shape: PropTypes.oneOf(["normal", "rounded", "circular"]),
   variant: PropTypes.oneOf(["normal", "outlined", "soft"]),
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {Button} from "./components/ui/button/index.js";
+import {PlusIcon} from "@heroicons/react/24/outline/index.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className="mx-auto w-fit">
-      <Button>Save</Button>
+      <Button size="xs">Save</Button>
+      <Button size="sm">Save</Button>
+      <Button size="md">Save</Button>
+      <Button size="lg">Save</Button>
+      <Button size="xl">Save</Button>
+    </div>
+    <div className="mx-auto w-fit">
+      <Button size="xs" shape="rounded">Save</Button>
+      <Button size="sm" shape="rounded">Save</Button>
+      <Button size="md" shape="rounded">Save</Button>
+      <Button size="lg" shape="rounded">Save</Button>
+      <Button size="xl" shape="rounded">Save</Button>
+    </div>
+    <div className="mx-auto w-fit">
+      <Button size="xs" shape="circular"><PlusIcon className="size-6"/></Button>
+      <Button size="sm" shape="circular"><PlusIcon className="size-6"/></Button>
+      <Button size="md" shape="circular"><PlusIcon className="size-6"/></Button>
+      <Button size="lg" shape="circular"><PlusIcon className="size-6"/></Button>
+      <Button size="xl" shape="circular"><PlusIcon className="size-6"/></Button>
     </div>
   </StrictMode>,
 )
